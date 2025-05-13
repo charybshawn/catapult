ac10b64c18bae7881fe3d81f21586516fa38f63c
charybshawn <charybshawn@gmail.com>
2025-05-12 17:20:57 -0700

    Fix Weekly Planning page by replacing icon-empty component with standard SVG icons

--- resources/views/filament/pages/weekly-planning.blade.php
+++ resources/views/filament/pages/weekly-planning.blade.php
@@ -60,10 +60,9 @@
                         </div>
                     @else
                         <div class="p-6 text-center text-sm text-gray-500 dark:text-gray-400">
-                            <x-filament::icon-empty
-                                icon="heroicon-o-calendar"
-                                class="mx-auto h-12 w-12 text-gray-400 dark:text-gray-500 mb-4"
-                            />
+                            <svg class="mx-auto h-12 w-12 text-gray-400 dark:text-gray-500 mb-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
+                            </svg>
                             <p>No orders scheduled for harvest on this date.</p>
                         </div>
                     @endif
@@ -106,10 +105,9 @@
                         </div>
                     @else
                         <div class="p-6 text-center text-sm text-gray-500 dark:text-gray-400">
-                            <x-filament::icon-empty
-                                icon="heroicon-o-cube"
-                                class="mx-auto h-12 w-12 text-gray-400 dark:text-gray-500 mb-4"
-                            />
+                            <svg class="mx-auto h-12 w-12 text-gray-400 dark:text-gray-500 mb-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
+                            </svg>
                             <p>No products to harvest on this date.</p>
                         </div>
                     @endif
@@ -168,10 +166,9 @@
                 </div>
             @else
                 <div class="p-6 text-center text-sm text-gray-500 dark:text-gray-400">
-                    <x-filament::icon-empty
-                        icon="heroicon-o-flag"
-                        class="mx-auto h-12 w-12 text-gray-400 dark:text-gray-500 mb-4"
-                    />
+                    <svg class="mx-auto h-12 w-12 text-gray-400 dark:text-gray-500 mb-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5" />
+                    </svg>
                     <p>No planting recommendations for this harvest date.</p>
                 </div>
             @endif
@@ -241,10 +238,9 @@
                 </div>
             @else
                 <div class="p-6 text-center text-sm text-gray-500 dark:text-gray-400">
-                    <x-filament::icon-empty
-                        icon="heroicon-o-beaker"
-                        class="mx-auto h-12 w-12 text-gray-400 dark:text-gray-500 mb-4"
-                    />
+                    <svg class="mx-auto h-12 w-12 text-gray-400 dark:text-gray-500 mb-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 01-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 014.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0112 15a9.065 9.065 0 00-6.23-.693L5 14.5m14.8.8l1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0112 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
+                    </svg>
                     <p>No active crops found.</p>
                 </div>
             @endif
